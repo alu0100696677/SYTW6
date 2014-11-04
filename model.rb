5c9b09e96816b78bc49d491aae7d1d2ff3c2b5d8
Belen Armas <alu0100696677@ull.edu.es>
2014-11-04 13:58:24 +0000

    Implementacion app y models

--- model.rb
+++ model.rb
@@ -16,46 +16,16 @@ class Visit
   property :created_at, DateTime
   property :ip, IPAddress
   property :country, String
+  property :city, String
 
   belongs_to :shortenedurl
 
-  after :create, :set_country
-
-  def set_country
-    xml = RestClient.get "http://api.hostip.info/get_xml.php?ip=#{ip}"
-    self.country  = XmlSimple.xml_in(xml.to_s, { 'ForceArray' => false})['Hostip']['countryAbbrev']
-    self.save
-  end
-
-  def self.count_days_bar(identifier,num_days)
-    visits = count_by_date_with(identifier,num_days)
-    data, labels = [], []
-    visits.each {|visit| data << visit[1]; labels << "#{visit[0].day}/#{visit[0].month}"}
-    "http://chart.apis.google.com/chart?chs=820x180&cht=bvs&chxt=x&chco=a4b3f4&chm=N,000000,0,-1,11&chxl=0:|#{labels.join('|')}&chds=0,#{data.sort.last+10}&chd=t:#{data.join(',')}"
-  end
-
-  def self.count_country_chart(identifier,map)
-    countries, count = [], []
-    count_by_country_with(identifier).each {|visit| countries << visit.country; count << visit.count }
-    chart = {}
-    chart[:map] = "http://chart.apis.google.com/chart?chs=440x220&cht=t&chtm=#{map}&chco=FFFFFF,a4b3f4,0000FF&chld=#{countries.join('')}&chd=t:#{count.join(',')}"
-    chart[:bar] = "http://chart.apis.google.com/chart?chs=320x240&cht=bhs&chco=a4b3f4&chm=N,000000,0,-1,11&chbh=a&chd=t:#{count.join(',')}&chxt=x,y&chxl=1:|#{countries.reverse.join('|')}"
-    return chart
-  end
-
-  def self.count_by_date_with(identifier,num_of_days)
-    visits = repository(:default).adapter.query("SELECT date(created_at) as date, count(*) as count FROM visits where shortenedurl_urlshort = '#{identifier}' and created_at between CURRENT_DATE-#{num_of_days} and CURRENT_DATE+1 group by date(created_at)")
-    dates = (Date.today-num_of_days..Date.today)
-    results = {}
-    dates.each { |date|
-      visits.each { |visit| results[date] = visit.count if visit.date == date }
-      results[date] = 0 unless results[date]
-    }
-    results.sort.reverse
+  def self.date_with(identifier)
+    repository(:default).adapter.query("SELECT date(created_at) AS date , count(*) AS count FROM visits WHERE shortenedurl_urlshort = '#{identifier}' GROUP BY date")
   end
 
   def self.count_by_country_with(identifier)
-    repository(:default).adapter.query("SELECT country, count(*) as count FROM visits where shortenedurl_urlshort = '#{identifier}' group by country")
+    repository(:default).adapter.query("SELECT country, count(*) AS count FROM visits WHERE shortenedurl_urlshort = '#{identifier}' GROUP BY country")
   end
 
 end
